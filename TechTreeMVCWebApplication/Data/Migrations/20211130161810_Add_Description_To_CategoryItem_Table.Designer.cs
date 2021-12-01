92d9078d6bdae654e68e33d586d766b349443b08
me.saif <me.shaikhsaif@gmail.com>
2021-12-01 08:03:45 +0530

    Implementation of category items based on category.

--- TechTreeMVCWebApplication/Data/Migrations/20211130161810_Add_Description_To_CategoryItem_Table.Designer.cs
+++ TechTreeMVCWebApplication/Data/Migrations/20211130161810_Add_Description_To_CategoryItem_Table.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechTreeMVCWebApplication.Data;
 
 namespace TechTreeMVCWebApplication.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211130161810_Add_Description_To_CategoryItem_Table")]
+    partial class Add_Description_To_CategoryItem_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- TechTreeMVCWebApplication/Entities/CategoryItem.cs
+++ TechTreeMVCWebApplication/Entities/CategoryItem.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,8 +15,12 @@ namespace TechTreeMVCWebApplication.Entities
         [Required]
         [StringLength(100, MinimumLength = 3)]
         public string Title { get; set; }
+        public string Description { get; set; }
         public int CategoryId { get; set; }
         public int MediaTypeId { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<SelectListItem> MediaTypes { get; set; }
         public DateTime DateTimeReleased { get; set; }
 
     }
