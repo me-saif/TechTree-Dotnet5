0e1b8982f5488b8b6b9c404898222051bdc7af82
me.saif <me.shaikhsaif@gmail.com>
2021-12-01 09:44:44 +0530

    Implementation for category item edit operation

--- TechTreeMVCWebApplication/Areas/Admin/Controllers/CategoryItemController.cs
+++ TechTreeMVCWebApplication/Areas/Admin/Controllers/CategoryItemController.cs
@@ -96,11 +96,17 @@ namespace TechTreeMVCWebApplication.Areas.Admin.Controllers
                 return NotFound();
             }
 
+            List<MediaType> mediaTypes = await _context.MediaTypes.ToListAsync();
+
             var categoryItem = await _context.CategoryItems.FindAsync(id);
+
             if (categoryItem == null)
             {
                 return NotFound();
             }
+
+            categoryItem.MediaTypes = mediaTypes.ConvertToSelectList(categoryItem.MediaTypeId);
+
             return View(categoryItem);
         }
 
@@ -134,7 +140,7 @@ namespace TechTreeMVCWebApplication.Areas.Admin.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index), new { categoryId = categoryItem.CategoryId });
             }
             return View(categoryItem);
         }
@@ -165,7 +171,7 @@ namespace TechTreeMVCWebApplication.Areas.Admin.Controllers
             var categoryItem = await _context.CategoryItems.FindAsync(id);
             _context.CategoryItems.Remove(categoryItem);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Index), new { categoryId = categoryItem.CategoryId });
         }
 
         private bool CategoryItemExists(int id)
